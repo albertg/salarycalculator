c548c9b5f4f3c3b11e16ff48c0fe090fdd9860ec
Albert George <albert.george@outlook.com>
2019-08-10 16:37:54 +0530

    Removed redundant code

--- Remuneration.Lib/TaxVariables.cs
+++ Remuneration.Lib/TaxVariables.cs
@@ -2,15 +2,6 @@
 
 namespace Quantum.OS.Remuneration.Library {
     public class TaxVariables : ITaxVariables {
-        public const double TaxSlab1 = 5 / 100.00;
-        public const double TaxSlab2 = 20.00 / 100.00;
-        public const double TaxSlab3 = 30.00 / 100.00;
-        public const double CessOnTax = 4.00 / 100.00;
-        public const double Slab1 = 250000.00;
-        public const double Slab2 = 500000.00;
-        public const double Slab3 = 1000000.00;
-        public const double StandardDeduction = 50000.00;
-
         public double Slab1TaxRate => 5 / 100.00;
 
         public double Slab2TaxRate => 20.00 / 100.00;
